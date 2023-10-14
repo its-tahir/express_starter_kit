00ce122904da9f52d922e30353de4279135d4ac2
Tahir <i.tahirnazir@gmail.com>
2023-10-14 22:55:38 +0500

    mongoDb connection / mongoose

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
-
-// lode middlewares
-const logger = require('./middleware/logger')
+const connectDb = require('./config/db')
 
 // lode env vars
 dotenv.config({ path: './config/config.env' })
+
+// connect database
+connectDb();
+
+// lode middlewares
+const logger = require('./middleware/logger')
 
 //lode routes 
 const bootcamps = require('./router/bootcamps')
@@ -26,4 +29,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`));
+
+// handle unHandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // close server and exit process
+    server.close(() => process.exit(1))
+})
